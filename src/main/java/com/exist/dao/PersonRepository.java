20ff5b44489551b3329e1f1a93ea989e927a1753
JuliusPH <juliuscessarlapugot@gmail.com>
2016-07-14 10:59:59 +0800

    Add PersonRepository

--- src/main/java/com/exist/dao/PersonRepository.java
+++ src/main/java/com/exist/dao/PersonRepository.java
@@ -1,7 +1,15 @@
 package com.exist.dao;
 
-/**
- * Created by jlapugot on 7/14/16.
- */
-public interface PersonRepository {
+import com.exist.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PersonRepository extends JpaRepository<Person, Long>{
+    Person findOne(Long id);
+
+    List<Person> findAll();
+
+    Person save(Person person);
+
+    void delete(Person person);
 }

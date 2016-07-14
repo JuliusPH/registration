468c4d6fcb5d41a263dca56d22548bae4d9ce05d
JuliusPH <juliuscessarlapugot@gmail.com>
2016-07-14 09:32:47 +0800

    Add entity Address

---
+++ src/main/java/com/exist/model/Address.java
@@ -0,0 +1,41 @@
+package com.exist.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "address")
+public class Address implements Serializable{
+    @Column(name = "street_number")
+    private String streetNumber;
+    @Column(name = "barangay")
+    private String barangay;
+    @Column(name = "city")
+    private String city;
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getBarangay() {
+        return barangay;
+    }
+
+    public void setBarangay(String barangay) {
+        this.barangay = barangay;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}

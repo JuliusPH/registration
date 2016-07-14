c8f60ca43248fa7016eb2fbd215db6eff63d7f9c
JuliusPH <juliuscessarlapugot@gmail.com>
2016-07-14 17:47:36 +0800

    Remove exclusion of Hibernate and Jpa auto config

--- src/main/java/com/exist/Application.java
+++ src/main/java/com/exist/Application.java
@@ -2,10 +2,8 @@ package com.exist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
+@SpringBootApplication
 public class Application{
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
